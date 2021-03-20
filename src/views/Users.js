c13e9d605eb5518c9ae10520619b2205e3f9495e
GrzegorzNecka <strzypior@gmail.com>
2021-03-20 20:18:22 +0100

    1 file modified

--- src/views/Users.js
+++ src/views/Users.js
@@ -5,14 +5,14 @@ const MyContext = React.createContext();
 const User = () => (
   <div>
     <MyContext.Consumer>
-      {context => <p> User name: ${context.name}</p>}
+      {context => <p> User name: {context.name}</p>}
     </MyContext.Consumer>
   </div>
 );
 
 const Users = () => (
   <div>
-    <MyContext.Provider value={{ name: "grzechu" }}>
+    <MyContext.Provider value={{ name: "Grzechu" }}>
       <h2 className="title is-3">Users</h2>
       <User />
     </MyContext.Provider>
