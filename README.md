f5d42d4a90167364751b568c869bb733eaee3956
GrzegorzNecka <strzypior@gmail.com>
2021-03-20 20:30:13 +0100

    2 files modified, 1 added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://github.com/eduwebpl/kurs-react-techniki-zaawansowane/tree/03/03/src/views
+
+- multistep zawiera wszystkie componenty należące do compound components
--- src/views/Users.js
+++ src/views/Users.js
@@ -3,19 +3,13 @@ import React, { useContext } from "react";
 const MyContext = React.createContext();
 
 
-const User = () => {
-  const context = useContext(MyContext);
-
-  return <div>{<p> User name: {context.name}</p>}</div>;
-};
-
 
 const Users = () => (
   <div>
-    <MyContext.Provider value={{ name: "Grzechu" }}>
+
       <h2 className="title is-3">Users</h2>
-      <User />
-    </MyContext.Provider>
+   
+
   </div>
 );
 
