b14651096ec8e9c1029368976675bfe0196e7acc
GrzegorzNecka <strzypior@gmail.com>
2021-03-20 21:32:44 +0100

    2 files modified

--- src/components/Multistep/Multistep.js
+++ src/components/Multistep/Multistep.js
@@ -1,7 +1,14 @@
 import React, { useState, useContext } from "react";
 
-const Page = ({ children }) => {
-  return <div>{children}</div>;
+const Page = ({ children, pageIndex }) => {
+  const { currentPage } = useContext(WizardContext);
+
+  /*
+    logika mówiąca, że jeśli strona jest aktywan to 
+    ma się wyświetlać a jeśli nie to ma zwracać null
+  */
+
+  return currentPage === pageIndex ? children : null;
 };
 
 const Controls = () => {
@@ -9,7 +16,7 @@ const Controls = () => {
   return (
     <div>
       <button onClick={() => changePage(currentPage - 1)}>Previous</button>
-      <button>Next</button>
+      <button onClick={() => changePage(currentPage + 1)}>Next</button>
       <button>Submit</button>
     </div>
   );
--- src/views/Users.js
+++ src/views/Users.js
@@ -5,9 +5,9 @@ const Users = () => (
   <div>
     <h2 className="title is-3">Users</h2>
     <Multistep.Wizard>
-      <Multistep.Page>page 1</Multistep.Page>
-      <Multistep.Page>page 2</Multistep.Page>
-      <Multistep.Page>page 3</Multistep.Page>
+      <Multistep.Page pageIndex={1}>page 1</Multistep.Page>
+      <Multistep.Page pageIndex={2}>page 2</Multistep.Page>
+      <Multistep.Page pageIndex={3}>page 3</Multistep.Page>
       <Multistep.Controls />
     </Multistep.Wizard>
   </div>
