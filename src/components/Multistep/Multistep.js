51355adb90b8b563ecf2d58adccc0d117b627cfe
GrzegorzNecka <strzypior@gmail.com>
2021-03-20 21:19:46 +0100

    1 file modified

--- src/components/Multistep/Multistep.js
+++ src/components/Multistep/Multistep.js
@@ -1,21 +1,42 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+
+const WizardContext = React.createContext({
+  currentPage: 1,
+  changePage: () => {}
+});
 
 const Page = ({ children }) => {
   return <div>{children}</div>;
 };
 
 const Controls = () => {
+  const context = useContext(WizardContext)
   return (
     <div>
-      <button>Previous</button>
+      <button onClick={}>Previous</button>
       <button>Next</button>
       <button>Submit</button>
     </div>
   );
 };
 
-const Multistep = ({ children }) => {
-  return <div>{children}</div>;
+const Wizard = ({ children }) => {
+  const [currentPage, setCurrentPage] = useState();
+
+  const changePage = newPageIndex => {
+    setCurrentPage(newPageIndex);
+  };
+
+  return (
+    <WizardContext.Provider
+      value={{
+        currentPage,
+        changePage
+      }}
+    >
+      {children}
+    </WizardContext.Provider>
+  );
 };
 
-export default { Multistep, Page, Controls };
+export { Wizard, Page, Controls };
